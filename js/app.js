81f29e44b2cb21a1fd4e59244f47a24ec84a1ada
fatihint <fatihint@gmail.com>
2018-08-31 19:11:02 +0300

    DataTables and edit, create pages

--- js/app.js
+++ js/app.js
@@ -36,5 +36,22 @@ $(document).ready(function () {
         $('.container-fluid').css('margin-top', height - 55 + "px");
         $('.sidebar').css('top', height);
     }
+
+    // Page subtitle align
+    let lineHeight = $('.page-subtitle').parent().height();
+    $('.page-subtitle').css('line-height',lineHeight + 'px');
     
+    // Additional table data
+    // Operation column for data table
+    let tableHeadHtml = '<th>Operation</th>';
+    $('#data-table thead th').last().after(tableHeadHtml)
+
+    // Operation column content, edit and delete buttons
+    let tableDataHtml = '<td><a href="edit.html" class="btn btn-sm btn-primary">Edit</a> <a href="" class="btn btn-sm btn-danger">Delete</a></td>';
+    $('tbody tr').append(tableDataHtml);
+
+    // Data Table initializer
+    $(document).ready(function () {
+        $('#data-table').DataTable();
+    });
 })
